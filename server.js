efee571eddb04504c27da8b6c62358b006bde4b5
TomasConti <tomasconti34@gmail.com>
2025-10-28 20:04:36 +0100

    Work in progress

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-import express from 'express';
+/*
+curl -X POST https://nodejsserver-l0yd.onrender.com/api/invert      -H "Content-Type: application/json"      -d '{"text":"Finocchietto"}'
+sudo apt install nodejs npm -y -> su ubuntu 22.04 installiamo ambiente run time nodejs e gestore pacchetti npm(simile a pip)
 
-const app = express();
-const PORT = 3000;
+npm init -> creo file fondamentale di ogni progetto Node.js: package.json.
 
-app.use(express.json());
+package.json: È un file di configurazione in formato JSON che contiene metadati sul 
+tuo progetto (nome, versione, autore, licenza) e, soprattutto, l'elenco di tutte 
+le dipendenze (le librerie esterne necessarie, come Express) e gli script di avvio.
 
+npm install express -> scarico express come frame work
+
+ node server.js -> facciamo partire tutto
+*/
+import express from 'express'; //importo express come libreria 
+const app = express(); //creo instanza app web 
+const PORT = 3000; //porta di ascolto e aggancio 
+app.use(express.json()); //middleware di gestione POST body in formato json
+//posso raccogliere le richieste con req.body
 // Log in memoria
 const invertLogs = [];
-
-// Serve una semplice pagina per vedere i log
+// La get su root deve restituire una semplice pagina di log al browser client 
 app.get('/', (req, res) => {
-  res.send(`
+  res.send(` //risposta alla get
     <h1>Log API /api/invert</h1>
     <pre>${invertLogs.join('\n')}</pre>
   `);
 });
-
 // API POST invert string
 app.post('/api/invert', (req, res) => {
-  const { text } = req.body;
+  const { text } = req.body; //prendo il body della request
   if (typeof text !== 'string') {
     return res.status(400).json({ error: 'Devi fornire una stringa in "text"' });
-  }
-  const reversed = text.split('').reverse().join('');
-  
+  } 
+  const reversed = text.split('').reverse().join('')+'_ok';
   // Salva il log
   invertLogs.push(`Input: ${text} → Output: ${reversed}`);
-  
-  res.json({ original: text, reversed });
+  res.json({ original: text, reversed });//json di risposta al client che chiama api
 });
-
-app.listen(PORT, () => {
-  console.log(`Server in ascolto su http://localhost:${PORT}`);
-});
+//bound applicazione
+app.listen(PORT, () => { console.log(`Server in ascolto su http://localhost:${PORT}`); });
